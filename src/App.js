7586d87145febf1c31943220084895f41a26d7db
Bishoy <Bishoy@gmail.com>
2024-07-17 13:49:13 +0300

    submit-challenge-2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+  const [currentDate, setDate] = useState(new Date());
+
+  function decreaseStep() {
+    setStep(step - 1);
+  }
+
+  function increaseStep() {
+    setStep(step + 1);
+  }
+
+  function decreaseCount() {
+    setCount(count - step);
+    setDate((currentDate) => {
+      const newDate = new Date(currentDate);
+      newDate.setDate(newDate.getDate() + step);
+      return newDate;
+    });
+  }
+
+  function increaseCount() {
+    setCount(count + step);
+    setDate((currentDate) => {
+      const newDate = new Date(currentDate);
+      newDate.setDate(newDate.getDate() + step);
+      return newDate;
+    });
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <>
+      <div className='container-app'>
+        <div className='step-container'>
+          <button onClick={decreaseStep}>-</button>
+          <p> Step : {step} </p>
+          <button onClick={increaseStep}>+</button>
+        </div>
+        <div className='count-container'>
+          <button onClick={decreaseCount}>-</button>
+          <p> count : {count} </p>
+          <button onClick={increaseCount}>+</button>
+        </div>
+
+        <p className='date'>
+          {count === 0 ? (
+            <p> current date is {currentDate.toDateString()} </p>
+          ) : (
+            <p>
+              {count} days from today is {currentDate.toDateString()}
+            </p>
+          )}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+    </>
   );
 }
 
